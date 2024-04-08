4e69bc82a7dea336d950606bfaf66ae825c1a233
zhacatomn <zhacatomn@gmail.com>
2024-04-08 11:25:48 +0800

    Add norvig predictions

--- run_m2.sh
+++ run_m2.sh
@@ -3,14 +3,14 @@
 # A bash script for running the M2 scorer on multiple files. Modify when needed.
 
 typomodel="gector+norvig"
-typotype=("light" "medium" "heavy")
+typotype=("long_sentence" "short_sentence")
 modeltype=("norvig" "norvig+gector")
 
 for typo in ${typotype[@]}; do
     m2file="./project/m2_annotations/ABCN.dev.gold.bea19_original_typo_$typo.txt"
     for model in ${modeltype[@]}; do
         echo -e "Running M2 on typo type ${typo} and model ${model}"
-        filename="./project/predictions/$typomodel/ABCN_original_typo_${typo}_$model.txt"
-        ./m2scorer/m2scorer $filename $m2file
+        filename="./project/predictions/$typomodel/ABCN_original_typo_${typo}_$model"
+        ./m2scorer/m2scorer "$filename.txt" $m2file > "${filename}_results.txt" 
     done
 done
