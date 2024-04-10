ea8d3afbf2c74d641477703fb4df15be1f5d0cf3
heeeyi <e0772644@u.nus.edu>
2024-04-10 14:57:42 +0800

    Generated graphics (in PDF and SVG) for typo-by-type analysis + Add a text file for observations & analysis, at project-typo_by_type_analysis-plots-plot_analysis.txt

--- project/typo_by_type_analysis/check_typo.py
+++ project/typo_by_type_analysis/check_typo.py
@@ -7,7 +7,6 @@ import difflib
 nltk.download('averaged_perceptron_tagger')
 nltk.download('punkt')
 
-
 # Get tag from TRUE CORRECT sentence
 def get_pos_tags(sentence):
     tokens = word_tokenize(sentence)
@@ -35,7 +34,6 @@ def get_pos_tags(sentence):
             pos_positions['inter'].append(idx)
     return pos_positions
 
-
 # M2 file is split by paragraphs
 def read_m2_file(m2_file_path):
     with open(m2_file_path, 'r') as f:
@@ -44,12 +42,10 @@ def read_m2_file(m2_file_path):
     paragraphs = [item.split('\n') for item in paragraphs_raw]
     return paragraphs
 
-
 def read_sentence_file(file_path):
     with open(file_path, 'r') as f:
         lines = f.readlines()
     return [line.strip() for line in lines]
-
 
 def check_equal_length(length_list):
     if len(length_list) <= 1:
@@ -57,7 +53,6 @@ def check_equal_length(length_list):
     res = all(elem == length_list[0] for elem in length_list)
 
     return res
-
 
 def evaluate_correction_accuracy(total_errors, corrected_errors):
     print("ACCURACY RATE")
@@ -70,7 +65,6 @@ def evaluate_correction_accuracy(total_errors, corrected_errors):
             accuracy_rate = -100
         print(f"{error_type.upper()}: {accuracy_rate:.2f}%")
 
-
 def process_files(m2_file, typo_file, true_corrected_file, my_corrected_file):
     m2_content = read_m2_file(m2_file)
     incorrect_content = read_sentence_file(typo_file)
@@ -78,13 +72,9 @@ def process_files(m2_file, typo_file, true_corrected_file, my_corrected_file):
     generated_content = read_sentence_file(my_corrected_file)
 
     # check equal length
-    length_list = [
-        len(m2_content),
-        len(incorrect_content),
-        len(correct_content),
-        len(generated_content)
-    ]
+    length_list = [len(m2_content), len(incorrect_content), len(correct_content), len(generated_content)]
     if not check_equal_length(length_list):
+        print(length_list)
         print("Found disparity between lengths of different files")
     max_length = min(length_list)
 
@@ -92,16 +82,18 @@ def process_files(m2_file, typo_file, true_corrected_file, my_corrected_file):
     corrected_errors = defaultdict(int)
 
     for i in range(max_length):
-        evaluate_correction_results(total_errors, corrected_errors,
-                                    m2_content[i], incorrect_content[i],
-                                    correct_content[i], generated_content[i])
+        evaluate_correction_results(
+            total_errors, corrected_errors,
+            m2_content[i], incorrect_content[i],
+            correct_content[i], generated_content[i])
 
     return total_errors, corrected_errors
 
-
-def evaluate_correction_results(total_errors, corrected_errors, m2_list,
-                                original_sentence, true_corrected_sentence,
-                                my_corrected_sentence):
+def evaluate_correction_results(
+        total_errors, corrected_errors,
+        m2_list, original_sentence,
+        true_corrected_sentence,
+        my_corrected_sentence):
 
     pos_positions = get_pos_tags(true_corrected_sentence)
 
@@ -127,14 +119,13 @@ def evaluate_correction_results(total_errors, corrected_errors, m2_list,
     inter_total_error = 0
     inter_error_corrected = 0
 
-    # The difference in index (from original to corrected)
-    original_correct_offset = 0
+    original_correct_offset = 0 # The difference in index (from original to corrected)
     original_generate_offset = 0
 
-    s = difflib.SequenceMatcher(None, original_sentence.split(),
-                                my_corrected_sentence.split())
+    s = difflib.SequenceMatcher(None, original_sentence.split(), my_corrected_sentence.split())
     generate_align = s.get_opcodes()
     generate_align_ptr = 0
+
     '''
     (1) Get the offset between original and my_corrected sentence, using difflib
     (2) Get the offset between original and true_corrected sentence, using m2 operations
@@ -147,8 +138,7 @@ def evaluate_correction_results(total_errors, corrected_errors, m2_list,
         original_offset = int(correction.split()[1])
 
         # get original_generate_offset
-        while generate_align_ptr < len(generate_align) and generate_align[
-                generate_align_ptr][2] < original_offset:
+        while generate_align_ptr < len(generate_align) and generate_align[generate_align_ptr][2] < original_offset:
             tag, i1, i2, j1, j2 = generate_align[generate_align_ptr]
 
             original_generate_offset += (j2 - j1) - (i2 - i1)
@@ -157,11 +147,11 @@ def evaluate_correction_results(total_errors, corrected_errors, m2_list,
         # get original_correct_offset (but should do the addition by END of each loop)
         try:
             error_type = correction.split()[2].split("|||")[1]
-            if error_type == "noop":  # i.e. no operation
+            if error_type == "noop": # i.e. no operation
                 continue
 
-            correction_type = error_type.split(":")[0]  # e.g. M, R, U
-            word_type = error_type.split(":")[1]  # e.g. PREP, SPELL
+            correction_type = error_type.split(":")[0] # e.g. M, R, U
+            word_type = error_type.split(":")[1] # e.g. PREP, SPELL
         except:
             print("Error retrieving correction_type")
             print(correction)
@@ -178,8 +168,7 @@ def evaluate_correction_results(total_errors, corrected_errors, m2_list,
                 if generate_offset >= len(my_corrected):
                     # Actually this would also count towards failed correction -- sample index: Corrected:  (57, 55) Generated:  (51, 51)
                     pass
-                elif true_corrected[correct_offset].lower(
-                ) == my_corrected[generate_offset].lower():
+                elif true_corrected[correct_offset].lower() == my_corrected[generate_offset].lower():
                     verb_error_corrected += 1
 
             if correct_offset in pos_positions['prep']:
@@ -187,8 +176,7 @@ def evaluate_correction_results(total_errors, corrected_errors, m2_list,
 
                 if generate_offset >= len(my_corrected):
                     pass
-                elif true_corrected[correct_offset].lower(
-                ) == my_corrected[generate_offset].lower():
+                elif true_corrected[correct_offset].lower() == my_corrected[generate_offset].lower():
                     prep_error_corrected += 1
 
             if correct_offset in pos_positions['noun']:
@@ -196,8 +184,7 @@ def evaluate_correction_results(total_errors, corrected_errors, m2_list,
 
                 if generate_offset >= len(my_corrected):
                     pass
-                elif true_corrected[correct_offset].lower(
-                ) == my_corrected[generate_offset].lower():
+                elif true_corrected[correct_offset].lower() == my_corrected[generate_offset].lower():
                     noun_error_corrected += 1
 
             if correct_offset in pos_positions['adj']:
@@ -205,8 +192,7 @@ def evaluate_correction_results(total_errors, corrected_errors, m2_list,
 
                 if generate_offset >= len(my_corrected):
                     pass
-                elif true_corrected[correct_offset].lower(
-                ) == my_corrected[generate_offset].lower():
+                elif true_corrected[correct_offset].lower() == my_corrected[generate_offset].lower():
                     adj_error_corrected += 1
 
             if correct_offset in pos_positions['adv']:
@@ -214,8 +200,7 @@ def evaluate_correction_results(total_errors, corrected_errors, m2_list,
 
                 if generate_offset >= len(my_corrected):
                     pass
-                elif true_corrected[correct_offset].lower(
-                ) == my_corrected[generate_offset].lower():
+                elif true_corrected[correct_offset].lower() == my_corrected[generate_offset].lower():
                     adv_error_corrected += 1
 
             if correct_offset in pos_positions['pron']:
@@ -223,8 +208,7 @@ def evaluate_correction_results(total_errors, corrected_errors, m2_list,
 
                 if generate_offset >= len(my_corrected):
                     pass
-                elif true_corrected[correct_offset].lower(
-                ) == my_corrected[generate_offset].lower():
+                elif true_corrected[correct_offset].lower() == my_corrected[generate_offset].lower():
                     pron_error_corrected += 1
 
             if correct_offset in pos_positions['conj']:
@@ -232,8 +216,7 @@ def evaluate_correction_results(total_errors, corrected_errors, m2_list,
 
                 if generate_offset >= len(my_corrected):
                     pass
-                elif true_corrected[correct_offset].lower(
-                ) == my_corrected[generate_offset].lower():
+                elif true_corrected[correct_offset].lower() == my_corrected[generate_offset].lower():
                     conj_error_corrected += 1
 
             if correct_offset in pos_positions['inter']:
@@ -241,8 +224,7 @@ def evaluate_correction_results(total_errors, corrected_errors, m2_list,
 
                 if generate_offset >= len(my_corrected):
                     pass
-                elif true_corrected[correct_offset].lower(
-                ) == my_corrected[generate_offset].lower():
+                elif true_corrected[correct_offset].lower() == my_corrected[generate_offset].lower():
                     inter_error_corrected += 1
         except Exception as e:
             print(e)
@@ -252,11 +234,25 @@ def evaluate_correction_results(total_errors, corrected_errors, m2_list,
             print("Error comparing correction due to offset out of bound")
 
         # Update original_correct_offset
-        if correction_type == "M":
-            original_correct_offset += 1
+        try:
+            start_replace = int(correction.split()[1])
+            end_replace = int(correction.split()[2].split("|||")[0])
 
-        elif correction_type == "U":
-            original_correct_offset -= 1
+            new_words = correction.split("|||")[2].split() # cannot first split by space, otherwise multiple words also splitted
+
+            if correction_type == "M":
+                original_correct_offset += len(new_words)
+
+            elif correction_type == "U":
+                original_correct_offset -= (end_replace - start_replace)
+
+            elif correction_type == "R":
+                original_correct_offset += len(new_words) - (end_replace - start_replace)
+
+        except:
+            print("Error retrieving start & end replacement number")
+            print(correction)
+            continue
 
     # finally, update the dict
     total_errors['verb'] += verb_total_error
@@ -279,19 +275,21 @@ def evaluate_correction_results(total_errors, corrected_errors, m2_list,
 
 
 # Usage:
-folder_path = ''
+folder_path = '/content/drive/My Drive/cs2109_dataset/cs4248_assignment2/typo/'
 
-m2_file = folder_path + \
-    "project/m2_annotations/ABCN.dev.gold.bea19_original_typo.txt"
-typo_file = folder_path + \
-    "project/data/original_typo/long_sentence/ABCN.dev.gold.bea19_original_typo.txt"
-true_corrected_file = folder_path + \
-    "project/data/corrected/corrected_long_sentence/ABCN.dev.gold.bea19_corrected_long_sentence.txt"
-my_corrected_file = folder_path + \
-    "project/predictions/gector+bart/reduced_overcorrections/variants/bart+gector/ABCN_dev_original_typo_bart+gector_long_sentence.txt"
+m2_file = folder_path + "true.m2"
+typo_file = folder_path + "ABCN.dev.gold.bea19_original_typo.txt"
+true_corrected_file = folder_path + "ABCN.dev.gold.bea19_corrected.txt"
+my_corrected_file = folder_path + "ABCN_original_typo_norvig.txt"
 
-total_errors, corrected_errors = process_files(m2_file, typo_file,
-                                               true_corrected_file,
-                                               my_corrected_file)
+total_errors, corrected_errors = process_files(
+    m2_file, typo_file, true_corrected_file, my_corrected_file)
 
 evaluate_correction_accuracy(total_errors, corrected_errors)
+
+    
+
+
+
+
+
